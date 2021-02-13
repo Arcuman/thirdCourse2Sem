82961223c06169d9cd47b749355414edc4e29157
Arcuman <anton.arcuman.borisov@gmail.com>
2021-02-13 23:54:54 +0300

    feat: add almost 2 lab on database

---
+++ DataBase/Lab2/Lab2/DB.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace Lab2
+{
+    class DB
+    {
+        SqlConnection conn;
+        public void openConnection(string connStr)
+        {
+            conn = new SqlConnection(connStr);
+            conn.Open();
+        }
+
+        public void closeConnection()
+        {
+            conn.Close();
+        }
+
+        public void add_Product(string Name, int Price, int Quantity)
+        {
+            SqlCommand command = new SqlCommand("AddProduct", conn)
+            {
+                CommandType = CommandType.StoredProcedure
+            };
+            command.Parameters.Add("name", sqlDbType: SqlDbType.NVarChar).Value = Name;
+            command.Parameters.Add("price", sqlDbType: SqlDbType.Decimal).Value = Price;
+            command.Parameters.Add("count", sqlDbType: SqlDbType.NVarChar).Value = Quantity;
+            command.ExecuteNonQuery();
+        }
+
+        public void drop_Product(int id)
+        {
+            SqlCommand command = new SqlCommand("DeleteProduct", conn)
+            {
+                CommandType = CommandType.StoredProcedure
+            };
+            command.Parameters.Add("product_id", sqlDbType: SqlDbType.NVarChar).Value = id;
+            command.ExecuteNonQuery();
+        }
+
+        public void change_Product(int id, string Name, int Price, int Quantity)
+        {
+            SqlCommand command = new SqlCommand("UpdateProduct", conn)
+            {
+                CommandType = CommandType.StoredProcedure
+            };
+            command.Parameters.Add("product_id", sqlDbType: SqlDbType.Int).Value = id;
+            command.Parameters.Add("name", sqlDbType: SqlDbType.NVarChar).Value = Name;
+            command.Parameters.Add("price", sqlDbType: SqlDbType.Decimal).Value = Price;
+            command.Parameters.Add("quantity", sqlDbType: SqlDbType.Int).Value = Quantity;
+            command.ExecuteNonQuery();
+        }
+
+        // ----------------------------------------------------------------------------
+
+        public void add_Customer(string Fname, string Lname, string Email, string Address, string City)
+        {
+            SqlCommand command = new SqlCommand("AddCustomer", conn)
+            {
+                CommandType = CommandType.StoredProcedure
+            };
+            command.Parameters.Add("@first_name", sqlDbType: SqlDbType.NVarChar).Value = Fname;
+            command.Parameters.Add("@last_name", sqlDbType: SqlDbType.NVarChar).Value = Lname;
+            command.Parameters.Add("@email", sqlDbType: SqlDbType.NVarChar).Value = Email;
+            command.Parameters.Add("@address", sqlDbType: SqlDbType.NVarChar).Value = Address;
+            command.Parameters.Add("@city", sqlDbType: SqlDbType.NVarChar).Value = City;
+            command.ExecuteNonQuery();
+        }
+
+        public void drop_Customer(int id)
+        {
+            SqlCommand command = new SqlCommand("DeleteCustomerById", conn)
+            {
+                CommandType = CommandType.StoredProcedure
+            };
+            command.Parameters.Add("@customer_id", sqlDbType: SqlDbType.NVarChar).Value = id;
+            command.ExecuteNonQuery();
+        }
+
+        public void change_Customer(int id,string Fname, string Lname, string Email, string Address, string City)
+        {
+            SqlCommand command = new SqlCommand("UpdateCustomer", conn)
+            {
+                CommandType = CommandType.StoredProcedure
+            };
+            command.Parameters.Add("@customer_id", sqlDbType: SqlDbType.Int).Value = id;
+            command.Parameters.Add("@first_name", sqlDbType: SqlDbType.NVarChar).Value = Fname;
+            command.Parameters.Add("@last_name", sqlDbType: SqlDbType.NVarChar).Value = Lname;
+            command.Parameters.Add("@email", sqlDbType: SqlDbType.NVarChar).Value = Email;
+            command.Parameters.Add("@address", sqlDbType: SqlDbType.NVarChar).Value = Address;
+            command.Parameters.Add("@city", sqlDbType: SqlDbType.NVarChar).Value = City;
+            command.ExecuteNonQuery();
+        }
+
+        // ----------------------------------------------------------------------------
+
+        public int add_Order(int CumId, string RequiredDate, string[] ProductIds, string[] Quantities)
+        {
+
+            SqlCommand command = new SqlCommand("AddOrder", conn)
+            {
+                CommandType = CommandType.StoredProcedure
+            };
+            SqlParameter outputIdParam = new SqlParameter("@order_id", SqlDbType.Int)
+            {
+                Direction = ParameterDirection.Output
+            };
+            command.Parameters.Add(outputIdParam);
+            command.Parameters.Add("@order_date", sqlDbType: SqlDbType.Date).Value = DateTime.Now.ToLocalTime();
+            command.Parameters.Add("@required_date", sqlDbType: SqlDbType.Date).Value = DateTime.Parse(RequiredDate);
+            command.Parameters.Add("@customer_id", sqlDbType: SqlDbType.NVarChar).Value = CumId;
+            command.ExecuteNonQuery();
+
+            
+            for (int i =0; i < ProductIds.Length;i++)
+            {
+                command = new SqlCommand("AddOrderItem", conn)
+                {
+                    CommandType = CommandType.StoredProcedure
+                };
+                command.Parameters.Add("@order_id", sqlDbType: SqlDbType.Int).Value = (int)outputIdParam.Value;
+                command.Parameters.Add("@product_id", sqlDbType: SqlDbType.Int).Value = Convert.ToInt32(ProductIds[i]);
+                command.Parameters.Add("@quantity", sqlDbType: SqlDbType.Int).Value = Convert.ToInt32(Quantities[i]);
+                command.ExecuteNonQuery();
+            }
+            return (int)outputIdParam.Value;
+        }
+
+            public void drop_Order(int id)
+        {
+            string sql = "delete from Order where Id=" + id;
+            SqlCommand command = new SqlCommand(sql, conn);
+            command.ExecuteNonQuery();
+        }
+
+        public void change_Order(int id, int Status, string ShippedDate)
+        {
+            
+        }
+    }
+}
---
+++ DataBase/Lab2/Lab2/MainWindow.xaml.cs
@@ -0,0 +1,271 @@
+﻿using System;
+using System.Data;
+using System.Windows;
+using System.Data.SqlClient;
+
+namespace Lab2
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        string connStr = @"Data Source=DESKTOP-991U2T1\SQLEXPRESS;Initial Catalog=Store;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+        DataTable Products = new DataTable();
+        DataTable Order = new DataTable();
+        DataTable OrderItems = new DataTable();
+        DataTable Customer = new DataTable();
+
+        private void addProduct_Click(object sender, RoutedEventArgs e)
+        {
+            string Name = textBoxProductName.Text;
+            int Price = Convert.ToInt32(textBoxPrice.Text);
+            int Quantity = Convert.ToInt32(textBoxQuantity.Text);
+
+            if (Name.Length == 0)
+            {
+                MessageBox.Show("Проверьте данные");
+            }
+            else
+            {
+                DB db = new DB();
+                db.openConnection(connStr);
+                db.add_Product(Name, Price, Quantity);
+                MessageBox.Show("Выполнено !!!");
+                db.closeConnection();
+            }
+        }
+
+        private void dropProduct_Click(object sender, RoutedEventArgs e)
+        {
+            int id = Convert.ToInt32(textBoxId.Text);
+
+                DB db = new DB();
+                db.openConnection(connStr);
+                db.drop_Product(id);
+                MessageBox.Show("Выполнено !!!");
+                db.closeConnection();
+        }
+
+        private void changeProduct_Click(object sender, RoutedEventArgs e)
+        {
+            string Name = textBoxProductName.Text;
+            int Price = Convert.ToInt32(textBoxPrice.Text);
+            int Quantity = Convert.ToInt32(textBoxQuantity.Text);
+            int id = Convert.ToInt32(textBoxId.Text);
+
+            if (Name.Length == 0)
+            {
+                MessageBox.Show("Проверьте данные");
+            }
+            else
+            {
+                DB db = new DB();
+                db.openConnection(connStr);
+                db.change_Product(id, Name, Price, Quantity);
+                MessageBox.Show("Выполнено !!!");
+                db.closeConnection();
+            }
+        }
+
+        private void allProducts_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                string sqlExpression = "GetProducts";
+
+                using (SqlConnection connection = new SqlConnection(connStr))
+                {
+                    connection.Open();
+                    SqlDataAdapter command = new SqlDataAdapter(sqlExpression, connection);
+                    Products.Clear();
+                    command.Fill(Products);
+                    usersGrid.ItemsSource = Products.DefaultView;
+                    MessageBox.Show("Выполнено !!!");
+                    connection.Close();
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Ошибка запроса");
+            }
+        }
+        //--------------------------------------------------------------------------------------------------------------------
+
+        private void addCustomer_Click(object sender, RoutedEventArgs e)
+        {
+            string Fname = textBoxCFName.Text;
+            string Lname = textBoxCLName.Text;
+            string Email = textBoxEmail.Text;
+            string Address = textBoxAddress.Text;
+            string City = textBoxCity.Text;
+
+            if (Fname.Length == 0 || Lname.Length == 0)
+            {
+                MessageBox.Show("Проверьте данные");
+            }
+            else
+            {
+                DB db = new DB();
+                db.openConnection(connStr);
+                db.add_Customer(Fname, Lname, Email, Address, City);
+                MessageBox.Show("Выполнено !!!");
+                db.closeConnection();
+            }
+        }
+
+        private void dropCustomer_Click(object sender, RoutedEventArgs e)
+        {
+            int id = Convert.ToInt32(textBoxId1.Text);
+
+            DB db = new DB();
+            db.openConnection(connStr);
+            db.drop_Customer(id);
+            MessageBox.Show("Выполнено !!!");
+            db.closeConnection();
+        }
+
+        private void changeCustomer_Click(object sender, RoutedEventArgs e)
+        {
+            int id = Convert.ToInt32(textBoxId1.Text);
+            string Fname = textBoxCFName.Text;
+            string Lname = textBoxCLName.Text;
+            string Email = textBoxEmail.Text;
+            string Address = textBoxAddress.Text;
+            string City = textBoxCity.Text;
+
+
+            if (Fname.Length == 0 || Lname.Length == 0)
+            {
+                MessageBox.Show("Проверьте данные");
+            }
+            else
+            {
+                DB db = new DB();
+                db.openConnection(connStr);
+                db.change_Customer(id, Fname, Lname, Email, Address, City);
+                MessageBox.Show("Выполнено !!!");
+                db.closeConnection();
+            }
+        }
+        private void allCustomers_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                string sqlExpression = "GetCustomers";
+
+                using (SqlConnection connection = new SqlConnection(connStr))
+                {
+                    connection.Open();
+                    SqlDataAdapter command = new SqlDataAdapter(sqlExpression, connection);
+                    Customer.Clear();
+                    command.Fill(Customer);
+                    CustomerGrid.ItemsSource = Customer.DefaultView;
+                    MessageBox.Show("Выполнено !!!");
+                    connection.Close();
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Ошибка запроса");
+            }
+        }
+        //--------------------------------------------------------------------------------------------------------------------
+
+        private void addOrder_Click(object sender, RoutedEventArgs e)
+        {
+            string ProductIds = textProductIds.Text;
+            string Quantity = textQuantity.Text;
+            string[] ProductArr = ProductIds.Split(' ');
+            string[] Quantities = ProductIds.Split(' ');
+            string RequiredData = textBoxRequiredDate.Text;
+            int CumId = Convert.ToInt32(textBoxCumId.Text);
+            DB db = new DB();
+            db.openConnection(connStr);
+            MessageBox.Show(db.add_Order(CumId, RequiredData, ProductArr, Quantities).ToString());
+            db.closeConnection();
+        }
+
+        private void dropOrder_Click(object sender, RoutedEventArgs e)
+        {
+            int id = Convert.ToInt32(textBoxId2.Text);
+
+            DB db = new DB();
+            db.openConnection(connStr);
+            db.drop_Order(id);
+            MessageBox.Show("Выполнено !!!");
+            db.closeConnection();
+        }
+
+        private void changeOrder_Click(object sender, RoutedEventArgs e)
+        {
+            string OrderDate = textBoxOrderDate.Text;
+            string RequiredDate = textBoxRequiredDate.Text;
+            string ShippedDate = textShippedDate.Text;
+            int Status = Convert.ToInt32(textBoxStatus.Text);
+            int CumId = Convert.ToInt32(textBoxCumId.Text);
+            int id = Convert.ToInt32(textBoxId2.Text);
+
+            if (OrderDate.Length == 0 || RequiredDate.Length == 0)
+            {
+                MessageBox.Show("Проверьте данные");
+            }
+            else
+            {
+                DB db = new DB();
+                db.openConnection(connStr);
+                db.change_Order(id, Status, ShippedDate);
+                MessageBox.Show("Выполнено !!!");
+                db.closeConnection();
+            }
+        }
+        private void allOrders_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                string sqlExpression = "GetOrders";
+
+                using (SqlConnection connection = new SqlConnection(connStr))
+                {
+                    connection.Open();
+                    SqlDataAdapter command = new SqlDataAdapter(sqlExpression, connection);
+                    Order.Clear();
+                    command.Fill(Order);
+                    OrderGrid.ItemsSource = Order.DefaultView;
+                    MessageBox.Show("Выполнено !!!");
+                    connection.Close();
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Ошибка запроса");
+            }
+        }
+        private void allOrderItems_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                string sqlExpression = "GetOrderItems";
+
+                using (SqlConnection connection = new SqlConnection(connStr))
+                {
+                    connection.Open();
+                    SqlDataAdapter command = new SqlDataAdapter(sqlExpression, connection);
+                    OrderItems.Clear();
+                    command.Fill(OrderItems);
+                    OrderItemsGrid.ItemsSource = OrderItems.DefaultView;
+                    MessageBox.Show("Выполнено !!!");
+                    connection.Close();
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Ошибка запроса");
+            }
+        }
+    }
+}
